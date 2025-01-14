07e4f27f47afa66a28b6e966d8dbc1dcffa82dba
Cozmorry <cozmorry@gmail.com>
2025-01-14 10:42:12 +0300

    Numbers converter (with library imports)

--- numbers.py
+++ numbers.py
@@ -1,9 +1,9 @@
 from word2number import w2n
-def wordstonumber (text):
+def words_to_number(text):
     try: 
         return w2n.word_to_num(text)
     except ValueError as e:
         return f"Error: {e}"
 
-input_in_words = input ("Enter a number in words: ")
-print (f"{wordstonumber (input_in_words)}")
+input_in_words = input("Enter a number in words: ")
+print(f"Number: {words_to_number(input_in_words)}")
